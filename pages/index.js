6a7c3fd91dd7522088211f50857cad593f7e749e
PedroOficiati <Pedrooficiati@gmail.com>
2025-05-19 08:14:26 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import Head from 'next/head';
+import Link from "next/link";
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -14,31 +14,45 @@ export default function Home() {
           <img src="logo.jpeg" alt="logo" />
           <h1>SITE DE RECEITAS</h1>
           <p>SELECIONE A LETRA INICIAL DA SUA RECEITA:</p>
-          <p className="alfabeto">A-B-C-D-E-F-G-H-I-J-K-L-M-N-O-P-Q-R-S-T-U-V-W-X-Y-Z</p>
+          <p className="alfabeto">
+            A-B-C-D-E-F-G-H-I-J-K-L-M-N-O-P-Q-R-S-T-U-V-W-X-Y-Z
+          </p>
         </header>
 
         <section className="conteudo">
-          <div className="lista">
+          <article className="lista">
             <h2>RECEITAS COM A LETRA "A"</h2>
             <ul>
-              <li><Link href="/receita/1">Arroz à grega</Link></li>
-              <li><Link href="/receita/2">Abobrinha recheada</Link></li>
-              <li><Link href="/receita/3">Acarajé</Link></li>
-              <li><Link href="/receita/4">Açaí na tigela</Link></li>
-              <li><Link href="/receita/5">Almôndegas ao molho</Link></li>
+              <li>
+                <Link href="/receita/1">Arroz à grega</Link>
+              </li>
+              <li>
+                <Link href="/receita/2">Abobrinha recheada</Link>
+              </li>
+              <li>
+                <Link href="/receita/3">Acarajé</Link>
+              </li>
+              <li>
+                <Link href="/receita/4">Açaí na tigela</Link>
+              </li>
+              <li>
+                <Link href="/receita/5">Almôndegas ao molho</Link>
+              </li>
             </ul>
-          </div>
+          </article>
 
-          <div className="detalhes">
+          <article className="detalhes">
             <h2>AÇAÍ NA TIGELA</h2>
             <img src="acai.jpg" alt="Açaí na tigela" className="foto-receita" />
             <h3>INSTRUÇÕES:</h3>
             <p>
-              Para fazer o açaí na tigela, bata a polpa de açaí congelada no liquidificador com banana
-              e, se quiser, adoce com um pouco de xarope de guaraná. Depois, coloque o creme em uma tigela
-              e finalize com granola, fatias de banana e mel por cima. Sirva imediatamente.
+              Para fazer o açaí na tigela, bata a polpa de açaí congelada no
+              liquidificador com banana e, se quiser, adoce com um pouco de
+              xarope de guaraná. Depois, coloque o creme em uma tigela e
+              finalize com granola, fatias de banana e mel por cima. Sirva
+              imediatamente.
             </p>
-          </div>
+          </article>
         </section>
       </main>
     </>
